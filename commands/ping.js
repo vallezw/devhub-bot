a433c7cc5e29b6cc2e615aa86371b0d2b524d22a
Valentin Zwerschke <v.zwerschke@mail.de>
2022-06-14 00:09:33 +0200

    Add pong cmd

--- commands/ping.js
+++ commands/ping.js
@@ -2,48 +2,12 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js");
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("Replies with Pong!"),
+  data: new SlashCommandBuilder().setName("ping").setDescription("Pong?"),
   async execute(interaction) {
-	const row = new MessageActionRow().addComponents(
-		new MessageButton()
-		.setCustomId("primary")
-		.setLabel("Primary")
-		.setStyle("PRIMARY")
-  	);
-
-	const embed = new MessageEmbed()
-		.setColor("#0099ff")
-		.setTitle("Some title")
-		.setURL("https://discord.js.org")
-		.setDescription("Some description here");
-
-	await interaction.reply({
-		content: "Pong!",
-		ephemeral: true,
-		embeds: [embed],
-		components: [row],
-	});
-
-
-	const filter = (i) =>
-    i.customId === "primary" 
-
-  	const collector = interaction.channel.createMessageComponentCollector({
-		filter,
-		time: 15000,
-	});
-
-	collector.on("collect", async (i) => {
-		if (i.customId === "primary") {
-			await i.update({ content: "A button was clicked!", components: [] });
-		}
-	});
-
-	collector.on("end", (collected) =>
-		console.log(`Collected ${collected.size} items`)
-	);
-
+    return await interaction.reply({
+      content:
+        "Pong! Replied in `" + Math.round(interaction.client.ws.ping) + "ms`.",
+      ephemeral: true,
+    });
   },
 };
