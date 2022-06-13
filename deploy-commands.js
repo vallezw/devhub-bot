a8715ee0e4f8097fe8e0b8d63753934d521d7be0
Valentin Zwerschke <v.zwerschke@mail.de>
2022-06-13 02:00:52 +0200

    Refactor command handling

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,27 +1,30 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const dotenv = require('dotenv');
-const { clientId, guildId } = require('./config.json');
+const fs = require("node:fs");
+const path = require("node:path");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const dotenv = require("dotenv");
+
+const { clientId, guildId } = require("./config.json");
 dotenv.config();
 
 const token = process.env.DISCORD_TOKEN;
 
-const commands = [
-	new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with pong!'),
-	new SlashCommandBuilder()
-		.setName('server')
-		.setDescription('Replies with server info!'),
-	new SlashCommandBuilder()
-		.setName('user')
-		.setDescription('Replies with user info!'),
-].map((command) => command.toJSON());
+const commands = [];
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
 
-const rest = new REST({ version: '9' }).setToken(token);
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: "9" }).setToken(token);
 
 rest
-	.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+  .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+  .then(() => console.log("Successfully registered application commands."))
+  .catch(console.error);
--- index.js
+++ index.js
@@ -1,34 +1,48 @@
-// Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const { Client, Collection, Intents } = require('discord.js');
 const dotenv = require('dotenv');
+const fs = require("node:fs");
+const path = require("node:path");
 
 dotenv.config();
 const token = process.env.DISCORD_TOKEN;
 
-// Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
+
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  client.commands.set(command.data.name, command);
+}
+
+
+client.once("ready", () => {
+  console.log("Ready!");
 });
 
-client.on('interactionCreate', async (interaction) => {
-	if (!interaction.isCommand()) return;
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
 
-	const { commandName } = interaction;
+  const command = client.commands.get(interaction.commandName);
 
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	}
-	else if (commandName === 'server') {
-		await interaction.reply(
-      `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
-    );	}
-	else if (commandName === 'user') {
-		await interaction.reply(
-      `Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`
-    );	}
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
 });
 
 client.login(token);
