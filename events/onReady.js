fd6e332d41fab2b39ae73a4daf9a7ab10cb19513
Valentin Zwerschke <v.zwerschke@mail.de>
2022-06-14 02:31:22 +0200

    Add actity onReady

--- events/onReady.js
+++ events/onReady.js
@@ -4,11 +4,13 @@ module.exports = {
   name: "ready",
   once: true,
   execute(client) {
-    console.log(`Ready! Logged in as ${client.user.tag}`);
-
     if (!fs.existsSync("./data")) {
       fs.mkdirSync("./data");
       console.log("Created data directory");
     }
+
+    client.user.setActivity("for new listings", { type: "WATCHING" });
+
+    console.log(`Ready! Logged in as ${client.user.tag}`);
   },
 };
